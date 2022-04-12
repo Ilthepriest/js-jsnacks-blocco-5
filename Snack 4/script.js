8e20e26cce604c91642066107de7626ea54f1ee9
Ilthepriest <robertodelprete1993@libero.it>
2022-04-12 12:59:53 +0200

    correct Snack 4

--- Snack 4/script.js
+++ Snack 4/script.js
@@ -1,14 +1,17 @@
 /* Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
 
- const array1 = ["a", "b", "c"]
 
- const array2 = ["1", "2", "3"]
+function mergeArrays(array_1, array_2){
+    const newArray = []
+    if(array_1.lenght === array_2.lenght){
+        array_1.forEach((element, index) =>{
+            newArray.push(element)
+            newArray.push(array_2[index])
+        })
+        return newArray
+    }
+    
+}
 
- const copy = ["a", ...array2[0], "b", ...array2[1], "c", ...array2[2]];
-
- console.log(copy);
-
-
-
-
+console.log(mergeArrays(["a", "b", "c"], ["1", "2", "3"]));
